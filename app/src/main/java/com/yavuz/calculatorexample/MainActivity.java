851a7b2c67ecb7af581316c1bbea05e40d28594b
Necdet Yavuz AKTAS <aktasnecdetyavuz@gmail.com>
2022-08-08 14:47:31 +0300

    Project completed

---
+++ app/src/main/java/com/yavuz/calculatorexample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.yavuz.calculatorexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText number1Text;
+    EditText number2Text;
+    TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.resultText);
+    }
+
+    public void sum(View view){
+
+        int number1 = Integer.parseInt(number1Text.getText().toString());
+        int number2 = Integer.parseInt(number2Text.getText().toString());
+        int result = number1 + number2;
+
+        resultText.setText("Result : " + result);
+    }
+
+    public void subtract(View view){
+
+        int number1 = Integer.parseInt(number1Text.getText().toString());
+        int number2 = Integer.parseInt(number2Text.getText().toString());
+        int result = number1 - number2;
+
+        resultText.setText("Result : " + result);
+    }
+
+    public void multiply(View view){
+
+        int number1 = Integer.parseInt(number1Text.getText().toString());
+        int number2 = Integer.parseInt(number2Text.getText().toString());
+        int result = number1 * number2;
+
+        resultText.setText("Result : " + result);
+
+    }
+
+    public void divide(View view){
+
+        int number1 = Integer.parseInt(number1Text.getText().toString());
+        int number2 = Integer.parseInt(number2Text.getText().toString());
+        int result = number1 / number2;
+
+        resultText.setText("Result : " + result);
+    }
+}
